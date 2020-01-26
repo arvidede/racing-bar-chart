155700a9d0ef151aa4dc9368798dbfa9ef6d2157
Arvid Edenheim <arved490@student.liu.se>
2020-01-26 13:53:35 +0100

    Updates

---
+++ precache-manifest.f411a7ffe4e03d4cd3d2706110d129d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fd20f7c2c9d420fef5d0e95f889ad27",
+    "url": "/racing-bar-chart/index.html"
+  },
+  {
+    "revision": "4fb1a2da5d9365eaeb37",
+    "url": "/racing-bar-chart/static/css/main.3b88084b.chunk.css"
+  },
+  {
+    "revision": "9519b1ac2d4c4d823822",
+    "url": "/racing-bar-chart/static/js/2.b7fd760f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/racing-bar-chart/static/js/2.b7fd760f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4fb1a2da5d9365eaeb37",
+    "url": "/racing-bar-chart/static/js/main.1e9f08b6.chunk.js"
+  },
+  {
+    "revision": "c64a5f16ecdba9fe7e61",
+    "url": "/racing-bar-chart/static/js/runtime-main.4e8705f6.js"
+  }
+]);
